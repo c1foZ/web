9c16d4bae1e08ab937bd83532437fcf27f51dbb4
c1foZ <tomife2@gmail.com>
2020-11-11 11:41:14 +0100

    added logger

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
-import express from 'express';
- 
+const express = require('express');
+const path = require('path');
+const logger = require('./logger');
+
 const app = express();
- 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
- 
-app.listen(3000, () =>
-  console.log('Example app listening on port 3000!'),
-);
+const PORT = process.env.PORT || 3000;
+
+app.use(logger);
+app.use(express.static(path.join(__dirname, '..', 'frontend/')));
+
+app.listen(PORT, () => console.log('App listening on port: ' + PORT));
+
