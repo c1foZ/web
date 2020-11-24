5dc020d5991a8d6f27612fe2ffe8b8e2cdf0b1c7
c1foZ <tomife2@gmail.com>
2020-11-24 12:13:51 +0100

    deleted logger

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const path = require('path');
-const logger = require('./logger');
 const cars = [];
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//app.use(logger);
 app.use(express.static(path.join(__dirname, '..', 'frontend/')));
 app.use(express.json());
 
