37aef5585ba069b5adbc6bfa751091d57d567e09
c1foZ <tomife2@gmail.com>
2020-11-24 17:02:21 +0100

    handlebars

---
+++ backend/cars.js
@@ -0,0 +1,19 @@
+const cars = [
+	{
+		id: 1,
+		name: 'Lambo',
+		color: 'purple',
+	},
+	{
+		id: 2,
+		name: 'Porsche',
+		color: 'yellow',
+	},
+	{
+		id: 3,
+		name: 'Ferrari',
+		color: 'red',
+	},
+];
+
+module.exports = cars;
--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,53 @@
 const express = require('express');
+const uuid = require('uuid');
 const path = require('path');
-const cars = [];
+const exphbs = require('express-handlebars');
+const cars = require('./cars');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '..', 'frontend/')));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const idFilter = (req) => (car) => car.id === parseInt(req.params.id);
 
 app.get('/cars', (req, res) => {
 	res.json(cars);
 });
 
 app.get('/cars/:id', (req, res) => {
-	const carID = Number(req.params.id);
-	const getCar = cars.find((car) => car.id === carID);
-	res.json(getCar);
+	const found = cars.some(idFilter(req));
+
+	if (found) {
+		res.json(cars.filter(idFilter(req)));
+		//res.status(200).json
+	} else {
+		res.status(400).json({
+			msg: `No car with the id of ${req.params.id}.`,
+		});
+	}
 });
 
 app.post('/cars', (req, res) => {
-	const nextCar = req.body;
+	const nextCar = { ...req.body, id: uuid.v4() };
+	if (!nextCar.name || !nextCar.color) {
+		return res.status(400).json({ msg: 'Please include a name and color' });
+	}
 	cars.push(nextCar);
-	res.json(cars);
+	res.redirect('/form.html');
 });
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/form.html', (req, res) => {
+	res.render('home', {
+		title: 'Registered cars',
+		cars,
+	});
+});
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log('App listening on port: ' + PORT));
