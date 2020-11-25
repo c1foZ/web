47fb507266a86f6e786d472e0560f4ad14db756f
c1foZ <tomife2@gmail.com>
2020-11-25 09:55:26 +0100

    add routes

---
+++ backend/routes/cars.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const uuid = require('uuid');
+const router = express.Router();
+const cars = require('../Cars');
+
+const idFilter = (req) => (car) => car.id === parseInt(req.params.id);
+
+router.get('/', (req, res) => {
+	res.json(cars);
+});
+
+router.get('/:id', (req, res) => {
+	const found = cars.some(idFilter(req));
+
+	if (found) {
+		res.json(cars.filter(idFilter(req)));
+	} else {
+		res.status(400).json({
+			msg: `No car with the id of ${req.params.id}.`,
+		});
+	}
+});
+
+router.post('/', (req, res) => {
+	const nextCar = { ...req.body, id: uuid.v4() };
+	if (!nextCar.name || !nextCar.color) {
+		return res.status(400).json({ msg: 'Please include a name and color' });
+	}
+	cars.push(nextCar);
+	res.redirect('/form.html');
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,13 @@
 const express = require('express');
-const uuid = require('uuid');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const cars = require('./cars');
+const cars = require('./Cars');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '..', 'frontend/')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-const idFilter = (req) => (car) => car.id === parseInt(req.params.id);
-
-app.get('/cars', (req, res) => {
-	res.json(cars);
-});
-
-app.get('/cars/:id', (req, res) => {
-	const found = cars.some(idFilter(req));
-
-	if (found) {
-		res.json(cars.filter(idFilter(req)));
-		//res.status(200).json
-	} else {
-		res.status(400).json({
-			msg: `No car with the id of ${req.params.id}.`,
-		});
-	}
-});
-
-app.post('/cars', (req, res) => {
-	const nextCar = { ...req.body, id: uuid.v4() };
-	if (!nextCar.name || !nextCar.color) {
-		return res.status(400).json({ msg: 'Please include a name and color' });
-	}
-	cars.push(nextCar);
-	res.redirect('/form.html');
-});
+app.use('/cars', require('./routes/cars'));
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -47,6 +18,8 @@ app.get('/form.html', (req, res) => {
 		cars,
 	});
 });
+
+app.use(express.static(path.join(__dirname, '..', 'frontend/')));
 
 const PORT = process.env.PORT || 3000;
 
